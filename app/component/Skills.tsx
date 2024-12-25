a2ebde713a4fe229bdc4277fe34330e08cac0a1c
hoangphi03 <nguyenhoangphi.hgi@gmail.com>
2024-12-25 23:00:14 +0700

    modify skills

--- app/component/Skills.tsx
+++ app/component/Skills.tsx
@@ -8,21 +8,23 @@ const Skills = () => {
       </h1>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-7 text-center gap-4 *:p-2 *:rounded-lg *:border *:bg-blue-50">
         <div className=" bg-opacit-90">HTML & CSS</div>
-        <div className=" bg-opacit-90">JavaScript</div>
         <div className=" bg-opacit-90">TypeScript</div>
+        <div className=" bg-opacit-90">JavaScript</div>
         <div className=" bg-opacit-90">React</div>
         <div className=" bg-opacit-90">NextJS</div>
-        <div className=" bg-opacit-90">NodeJs</div>
-        <div className=" bg-opacit-90">ExpressJs</div>
-        <div className=" bg-opacit-90">Mongodb</div>
-        <div className=" bg-opacit-90">Mysql</div>
-        <div className=" bg-opacit-90">Sql Server</div>
+        <div className=" bg-opacit-90 hidden">NodeJs</div>
+        <div className=" bg-opacit-90  hidden">ExpressJs</div>
+        <div className=" bg-opacit-90 hidden">Mongodb</div>
+        <div className=" bg-opacit-90 hidden">Mysql</div>
+        <div className=" bg-opacit-90 hidden">Sql Server</div>
         <div className=" bg-opacit-90">Tailwind css</div>
         <div className=" bg-opacit-90">Bootstrap</div>
         <div className=" bg-opacit-90">Figma</div>
         <div className=" bg-opacit-90">PhotoShop</div>
         <div className=" bg-opacit-90">Git & Github</div>
-        <div className=" bg-opacit-90">Linux</div>
+        <div className=" bg-opacit-90 hidden">Linux</div>
+        <div className=" bg-opacit-90 ">Responsive</div>
+        <div className=" bg-opacit-90 ">Chart.js</div>
       </div>
     </div>
   );
